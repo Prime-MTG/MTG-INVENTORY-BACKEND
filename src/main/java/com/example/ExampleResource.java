bab12b42bf921ab0847de60e9ec439377049abeb
Jhorner22 <jphorner22@gamil.com>
2023-08-10 22:08:47 -0500

    JDBC connection

--- src/main/java/com/example/ExampleResource.java
+++ src/main/java/com/example/ExampleResource.java
@@ -1,16 +1,30 @@
 package com.example;
 
+import io.agroal.api.AgroalDataSource;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 @Path("/hello")
 public class ExampleResource {
 //COMMENT
+    @Inject
+    AgroalDataSource agroalDataSource;
+
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
+    public String hello() throws SQLException {
+        Connection con = agroalDataSource.getConnection();
+        Statement statement = con.createStatement();
+        statement.execute("INSERT INTO roles(role_id, role_name) VALUES (2, 'Bill');");
+
         return "Hello from RESTEasy Reactive";
     }
 }
