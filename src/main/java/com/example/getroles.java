8a109e16160cd9958b722d7b71fb07a634c5de0f
Jhorner22 <jphorner22@gamil.com>
2023-08-24 23:14:41 -0500

    test SELECT endpoint

--- src/main/java/com/example/getroles.java
+++ src/main/java/com/example/getroles.java
@@ -12,18 +12,21 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-@Path("/hello")
-public class ExampleResource {
+@Path("/getroles")
+public class getroles {
+
     @Inject
     AgroalDataSource agroalDataSource;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() throws SQLException {
+    public String getroles() throws SQLException {
         Connection con = agroalDataSource.getConnection();
         Statement statement = con.createStatement();
-        statement.execute("INSERT INTO roles(role_id, role_name) VALUES (3, 'Tedd');");
-
-        return "Hello from RESTEasy Reactive";
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM roles;");
+        while (resultSet.next()) {
+            return resultSet.getString("role_name");
+        }
+        return "hello";
     }
 }
