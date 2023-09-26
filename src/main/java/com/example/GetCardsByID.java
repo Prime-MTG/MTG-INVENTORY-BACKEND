3649372469d179335503e52626a8264fe1b6691d
Jhorner22 <jphorner22@gamil.com>
2023-09-25 20:44:28 -0500

    changes endpoint to work with new schama

--- src/main/java/com/example/GetCardsByID.java
+++ src/main/java/com/example/GetCardsByID.java
@@ -5,6 +5,7 @@ import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
@@ -13,23 +14,24 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-@Path("/getroles")
-public class getroles {
+@Path("/getcardsbyuserid")
+public class GetCardsByID {
 
     @Inject
     AgroalDataSource agroalDataSource;
 
-    @ConfigProperty(name = "SQL.getfirstrole")
-    String getfirstrole;
+    @ConfigProperty(name = "SQL.getcardsbyuserid")
+    String getcardsbyuserid
+            ;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String getroles() throws SQLException {
+    public String getroles(@QueryParam("id") String id) throws SQLException {
         Connection con = agroalDataSource.getConnection();
         Statement statement = con.createStatement();
-        ResultSet resultSet = statement.executeQuery(getfirstrole);
+        ResultSet resultSet = statement.executeQuery(getcardsbyuserid);
         while (resultSet.next()) {
-            return resultSet.getString("role_name");
+            return resultSet.getString("CardID");
         }
         return "hello";
     }
